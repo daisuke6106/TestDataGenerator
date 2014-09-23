fb9cead018e35757c32c3103def475821821e59e
dk <daisuke6106@gmail.com>
2014-09-23 20:25:47 +0900

    日付範囲出力機能作成
    引数に指定された日付の範囲を標準出力に出力する機能を新規作成した

--- src/jp/co/dk/testdatagenerator/range/DRange.java
+++ src/jp/co/dk/testdatagenerator/range/DRange.java
@@ -1,5 +1,11 @@
 package jp.co.dk.testdatagenerator.range;
 
 public class DRange {
-
+	public static void main (String args[]) {
+		
+	}
+	
+	protected static String getManual() {
+		return "";
+	}
 }
